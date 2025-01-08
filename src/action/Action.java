804e35b0050dbf8b57d10989dafbff365fc5b513
genkichi <genshigoto0821@gmail.com>
2025-01-09 01:12:05 +0900

    java課題1_25 完成

--- src/action/Action.java
+++ src/action/Action.java
@@ -1,50 +1,98 @@
 package action;
-
+// Randomクラスを使用するためのインポート処理
 import java.util.Random;
+// Scannerクラスを使用するためのインポート処理
 import java.util.Scanner;
 
+// スーパークラスのインポート処理。
 import info.Info;
 
-
+/*
+ * [概要]サブクラスの作成。
+ * [詳細]サブクラスを作成し、スーパークラスのメソッドを継承する。
+ */
+// サブクラスの宣言。
 public class Action extends Info {
+	/*
+	 * [概要]コンストラクタの作成。
+	 * [詳細]コンストラクタを作成し、インスタンスを生成した時に最初に実行してほしい処理を記述する。
+	 */
+	// コンストラクタの作成。
 	public Action() {
+		// nameSettingメソッドの実行。
 		nameSetting();
+		// abilitynameメソッドの実行。
 		abilityname();
-		valuesetting();
+		// valuesettingメソッドの実行。
+		valueSetting();
+		// greetingメソッドの実行。
 		greeting();
 	}
-	
+	/*
+	 * [概要]メソッドの作成。
+	 * [詳細]名前をコンソール入力で受取り、親クラスのフィールド変数に値を代入するメソッドを作成。
+	 */
+	// メソッドの作成。
 	public void nameSetting() {
+		// Scannerクラスのインスタンス生成。
 		Scanner scanner = new Scanner(System.in);
+		// setterにscannerに入力された値を受け取る。
 		setName(scanner.nextLine());
+		// メモリの開放。
 		scanner.close();
 	}
-	
+	/*
+	 * [概要]メソッドの作成
+	 * [詳細]setterに、能力の種類を受け取るメソッドを作成する。
+	 */
+	// メソッドの作成
 	public void abilityname() {
+		// 配列を宣言、文字列を格納する。
 		String[] ability = {"HP", "MP", "攻撃力", "素早さ", "防御力"};
+		// setterに上記配列を受け取る。
 		setAbility(ability);
 	}
-	
-	public void valuesetting() {
+	/*
+	 * [概要]メソッドの作成
+	 * [詳細]乱数を能力の数だけ取得し、フィールド変数に格納する処理を記述する。
+	 */
+	// メソッドの作成。
+	public void valueSetting() {
+		// ランダムクラスのインスタンスを生成。
 		Random r = new Random();
+		// 能力値の個数だけ配列を作成する。
 		int[] value = new int[getAbility().length];
+		// for文。カウント用の変数をiとし、能力値の個数分だけ繰り返しを宣言。
 		for(int i = 0; i < getAbility().length; i++) {
+			// 配列に乱数を格納。(0~999)
 			value[i] = r.nextInt(999);
 		}
+		// setterを使用して、配列をフィールド変数に格納する。
 		setValue(value);
 	}
-	
+	/*
+	 * [概要]メソッドの作成
+	 * [詳細]コンソールに表示する文字列をまとめたメソッドを作成する。
+	 */
+	// メソッドの作成。
 	public void greeting() {
+		// getterを使用して名前を出力する。
 		System.out.println("こんにちは「" + getName() + "」さん");
+		// 表示する文字列
 		System.out.println("ステータス");
+		// for文 カウント用の変数iを0で初期化し、能力値の個数分だけ繰り返しを行う。
 		for(int i = 0; i < getAbility().length; i++) {
+			// getterを使用して、能力値の名前と、能力値を順番に出力する。
 			System.out.println(getAbility()[i] + " : " + getValue()[i]);
 		}
+		// 改行
 		System.out.println();
+		// 表示する文字列
 		System.out.println("さあ冒険に出かけよう！");
 	}
 	
 	public static void main(String[] args) {
+		// Actionクラスのインスタンスの生成。コンストラクタの実行。
 		Action act = new Action();
 	}
 }
--- src/info/Info.java
+++ src/info/Info.java
@@ -1,31 +1,52 @@
 package info;
-
+// スーパークラス。キャラクターの情報を格納する。
 public class Info {
+	/*
+	 * [概要]フィールド変数の宣言。
+	 * [詳細]必要なフィールド変数を用意する。また、カプセル化も行う。
+	 */
+	// キャラクター名を格納するフィールド変数を作成。
 	private String name;
+	// 能力名を格納するフィールド変数を作成。
 	private String[] ability;
+	// 能力値を格納するフィールド変数を作成。
 	private int[] value;
-	
+	/*
+	 * [概要]setterの作成。
+	 * [詳細]他クラスから、フィールド変数を変更するためのsetterを作成する。
+	 */
+	// キャラクター名を格納するセッターを宣言。引数にString型を指定する。
 	public void setName(String name) {
+		// フィールド変数に引数を代入する。
 		this.name = name;
 	}
-	
+	// 能力名を格納するためのsetterを作成。 引数にString型の配列を指定。
 	public void setAbility(String[] ability) {
+		// フィールド変数に引数を代入する。
 		this.ability = ability;
 	}
-	
+	// 能力値を格納するためのsetterを作成。 引数にint型の配列を指定。
 	public void setValue(int[] value) {
+		// フィールド変数に引数を代入する。
 		this.value = value;
 	}
-	
+	/*
+	 * [概要]getterの作成処理
+	 * [詳細]getterを作成し、他クラスからフィールド変数の値を参照出来るようにする。
+	 */
+	// getterの作成。戻り値の値をString型に指定。
 	public String getName() {
+		// フィールド変数nameを戻り値に指定。
 		return this.name;
 	}
-	
+	// getterの作成。戻り値の値をString型の配列に指定。
 	public String[] getAbility() {
+		// フィールド変数abilityを戻り値に指定。
 		return this.ability;
 	}
-	
+	// getterの作成。戻り値にint型の配列を指定。
 	public int[] getValue() {
+		// フィールド変数valueを戻り値に指定。
 		return this.value;
 	}
 }
